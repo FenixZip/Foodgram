69fa940a3ec7f0511fb49883e8e6eeea94bef9c5
Oleg Sorokin <fenix15@inbox.ru>
2025-03-23 13:38:28 +0300

    add model category

--- recipe_site/recipes/models.py
+++ recipe_site/recipes/models.py
@@ -1,2 +1,13 @@
 from django.db import models
 
+
+class Category(models.Model):
+    """Модель для хранения категорий: завтрак, обед, ужин"""
+    name = models.CharField(
+                            max_length=100,
+                            unique=True, db_index=True,
+                            verbose_name='Название категории')
+
+    def __str__(self):
+        """Отображение категорие по ее названию"""
+        return f'Категория -> {self.name}'
